d6898e215f1a85c904207365a2c2fd11f72bac8d
Rugved332 <103026921+Rugved332@users.noreply.github.com>
2024-03-29 16:09:16 +0200

    initial commit

---
+++ Assets/Scripts/DependencyInjection/Provider.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace DependencyInjection
+{
+    public class Provider : MonoBehaviour, IDependencyProvider
+    {
+
+    }
+}
